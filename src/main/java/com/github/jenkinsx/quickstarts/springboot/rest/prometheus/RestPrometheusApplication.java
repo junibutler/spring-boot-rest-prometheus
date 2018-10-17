98fc3176b79327ddd27ed4485d71484a5cd1c146
junibutler <jmukherjee@cloudbees.com>
2018-10-17 09:49:15 -0700

    hello from add

--- src/main/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplication.java
+++ src/main/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplication.java
@@ -24,7 +24,7 @@ public class RestPrometheusApplication {
 	@ResponseBody
 	public Map<String, Object> landingPage() {
 		Counter.builder("mymetric").tag("foo", "bar").register(registry).increment();
-        return singletonMap("hello", "kube cd");
+        return singletonMap("hello", "are you coming to nice, france for DWJW?");
 	}
 
 	public static void main(String[] args) {
